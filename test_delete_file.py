3097da202cf00ec08b395c2388e3fd26a8111d97
osakoh <macmive@gmail.com>
2022-12-18 20:41:56 +0000

    uncommented previous classes

--- test_delete_file.py
+++ test_delete_file.py
@@ -13,11 +13,8 @@ iv) test runner: a component which orchestrates the execution of tests and provi
 The runner may use a graphical interface, a textual interface, or return a
 special value to indicate the results of executing the tests.
 """
-# print(f"\n*********************************************************")
-# tmpFilePath: C:\Users\<username>\AppData\Local\Temp\tmp-testfile.txt
-# print(f"tmpFilePath: {tmpFilePath}")
-# print(f"*********************************************************\n")
-
+import os
+import tempfile
 # Unlike a Unix shell, Python does not do any automatic path expansions
 # path parameters are either strings or bytes
 # creates temporary files and directories consisting of,
@@ -28,62 +25,68 @@ from unittest import mock
 from file_deletion import file_delete
 
 
-# class FileDeleteTestCase(unittest.TestCase):
-#     """
-#     tempfile.gettempdir(): returns the name of the directory used for temporary files
-#     On Windows: directories, C:\\TEMP, C:\\TMP, \\TEMP, and \\TMP, in that order.
-#     Other platforms, the directories /tmp, /var/tmp, and /usr/tmp, in that order
-#
-#     Problems with this implementation style:
-#     i) a temporary file is created and then deleted each time the test is run.
-#     ii) no way of testing whether the file_delete method properly passes the argument down to the os.remove() call
-#     """
-#     tmpFilePath = os.path.join(tempfile.gettempdir(), "tmp-testfile.txt")
-#
-#     # instructions to be executed before each test
-#     def setUp(self) -> None:
-#         with open(self.tmpFilePath, "w") as f:
-#             f.write("Content of file")
-#
-#     def test_file_delete(self):
-#         """ tests file deletion method """
-#         file_delete(self.tmpFilePath)
-#         actual = os.path.isfile(self.tmpFilePath)
-#         expected = "Failed to remove the file."
-#         # Check that the expression is false
-#         self.assertFalse(actual, expected)
+# print(f"\n*********************************************************")
+# tmpFilePath: C:\Users\<username>\AppData\Local\Temp\tmp-testfile.txt
+# print(f"tmpFilePath: {tmpFilePath}")
+# print(f"*********************************************************\n")
 
 
-# class FileDeleteTestCaseWithMocking(unittest.TestCase):
-#     """
-#     tempfile.gettempdir(): returns the name of the directory used for temporary files
-#     On Windows: directories, C:\\TEMP, C:\\TMP, \\TEMP, and \\TMP, in that order.
-#     Other platforms, the directories /tmp, /var/tmp, and /usr/tmp, in that order
-#
-#     Adv:
-#     i) the os.remove() call in the file_delete method is tested with the right arguments
-#
-#     Problems:
-#     i) it's better to mock the os module itself rather than file_delete.os. For example, if the 'tempfile' module
-#     is to be mocked in this project, 'newProject.app.example_function', the mock will need to be applied to
-#     'newProject.app.tempfile', since each module has its own import.
-#     'file_delete' will have its own 'os' module at runtime.
-#     It's safer to mock an item where it is implemented, not where it came from
-#     """
-#     tmpFilePath = os.path.join(tempfile.gettempdir(), "tmp-testfile.txt")
-#
-#     # instructions to be executed before each test
-#     def setUp(self) -> None:
-#         with open(self.tmpFilePath, "w") as f:
-#             f.write("Content of file")
-#
-#     @mock.patch("file_deletion.os")
-#     def test_file_delete(self, mock_os_call):
-#         """ tests file deletion method """
-#         file_delete(self.tmpFilePath)
-#
-#         # check that the file_delete method called os.remove() with the correct arguments
-#         mock_os_call.remove.assert_called_with(self.tmpFilePath)
+class FileDeleteTestCase(unittest.TestCase):
+    """
+    tempfile.gettempdir(): returns the name of the directory used for temporary files
+    On Windows: directories, C:\\TEMP, C:\\TMP, \\TEMP, and \\TMP, in that order.
+    Other platforms, the directories /tmp, /var/tmp, and /usr/tmp, in that order
+
+    Problems with this implementation style:
+    i) a temporary file is created and then deleted each time the test is run.
+    ii) no way of testing whether the file_delete method properly passes the argument down to the os.remove() call
+    """
+    tmpFilePath = os.path.join(tempfile.gettempdir(), "tmp-testfile.txt")
+
+    # instructions to be executed before each test
+    def setUp(self) -> None:
+        with open(self.tmpFilePath, "w") as f:
+            f.write("Content of file")
+
+    def test_file_delete(self):
+        """ tests file deletion method """
+        file_delete(self.tmpFilePath)
+        actual = os.path.isfile(self.tmpFilePath)
+        expected = "Failed to remove the file."
+        # Check that the expression is false
+        self.assertFalse(actual, expected)
+
+
+class FileDeleteTestCaseWithMocking(unittest.TestCase):
+    """
+    tempfile.gettempdir(): returns the name of the directory used for temporary files
+    On Windows: directories, C:\\TEMP, C:\\TMP, \\TEMP, and \\TMP, in that order.
+    Other platforms, the directories /tmp, /var/tmp, and /usr/tmp, in that order
+
+    Adv:
+    i) the os.remove() call in the file_delete method is tested with the right arguments
+
+    Problems:
+    i) it's better to mock the os module itself rather than file_delete.os. For example, if the 'tempfile' module
+    is to be mocked in this project, 'newProject.app.example_function', the mock will need to be applied to
+    'newProject.app.tempfile', since each module has its own import.
+    'file_delete' will have its own 'os' module at runtime.
+    It's safer to mock an item where it is implemented, not where it came from
+    """
+    tmpFilePath = os.path.join(tempfile.gettempdir(), "tmp-testfile.txt")
+
+    # instructions to be executed before each test
+    def setUp(self) -> None:
+        with open(self.tmpFilePath, "w") as f:
+            f.write("Content of file")
+
+    @mock.patch("file_deletion.os")
+    def test_file_delete(self, mock_os_call):
+        """ tests file deletion method """
+        file_delete(self.tmpFilePath)
+
+        # check that the file_delete method called os.remove() with the correct arguments
+        mock_os_call.remove.assert_called_with(self.tmpFilePath)
 
 
 class FileDeleteTestCaseWithMockingOS(unittest.TestCase):
