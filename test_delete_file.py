3f33d2ecf7d61624a3370b3deb707cd2d9fc28b2
osakoh <macmive@gmail.com>
2022-12-18 14:22:20 +0000

    Âcleaned up test class

--- test_delete_file.py
+++ test_delete_file.py
@@ -7,12 +7,17 @@ ii) test case: represents the individual unit of testing. It checks for a specif
 set of inputs. unittest provides a base class, TestCase, which may be used to create new test cases.
 
 iii) test suite: is a collection of test cases, test suites, or both.
-It is used to aggregate tests that should be executed together.
+Used to aggregate tests that should be executed together.
 
-iv) test runner
-A test runner is a component which orchestrates the execution of tests and provides the outcome to the user.
-The runner may use a graphical interface, a textual interface, or return a special value to indicate the results of executing the tests.
+iv) test runner: a component which orchestrates the execution of tests and provides the outcome to the user.
+The runner may use a graphical interface, a textual interface, or return a
+special value to indicate the results of executing the tests.
 """
+# print(f"\n*********************************************************")
+# tmpFilePath: C:\Users\<username>\AppData\Local\Temp\tmp-testfile.txt
+# print(f"tmpFilePath: {tmpFilePath}")
+# print(f"*********************************************************\n")
+
 # Unlike a Unix shell, Python does not do any automatic path expansions
 # path parameters are either strings or bytes
 import os
@@ -25,18 +30,12 @@ from file_deletion import file_delete
 
 
 class FileDeleteTestCase(unittest.TestCase):
-    # original(not escaped): C:\TEMP, C:\TMP, \TEMP, & \TMP
-    # escaped backslash(\):  C:\\TEMP, C:\\TMP, \\TEMP, & \\TMP
     """
     tempfile.gettempdir(): returns the name of the directory used for temporary files
     On Windows: directories, C:\\TEMP, C:\\TMP, \\TEMP, and \\TMP, in that order.
     Other platforms, the directories /tmp, /var/tmp, and /usr/tmp, in that order
     """
     tmpFilePath = os.path.join(tempfile.gettempdir(), "tmp-testfile.txt")
-    print(f"\n*********************************************************")
-    # tmpFilePath: C|D:\Users\<username>\AppData\Local\Temp\tmp-testfile.txt
-    print(f"tmpFilePath: {tmpFilePath}")
-    print(f"*********************************************************\n")
 
     # instructions to be executed before each test
     def setUp(self) -> None:
